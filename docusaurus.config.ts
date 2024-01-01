1f301706eab71437dabf39113c510ddb82f259f5
Halcyon666 <jackchen541@sina.com>
2024-01-01 13:13:52 +0800

    format code

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,62 +1,61 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import '@docusaurus/theme-search-algolia';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import "@docusaurus/theme-search-algolia";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'halcyon666',
-  tagline: 'Enjoy writing, Happy a nice day!',
-  favicon: 'img/favicon.png',
+  title: "halcyon666",
+  tagline: "Enjoy writing, Happy a nice day!",
+  favicon: "img/favicon.png",
 
   // Set the production url of your site here
-  url: 'https://halcyon666.github.io',
+  url: "https://halcyon666.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/summary/',
+  baseUrl: "/summary/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'halcyon666', // Usually your GitHub org/user name.
-  projectName: 'summary', // Usually your repo name.
+  organizationName: "halcyon666", // Usually your GitHub org/user name.
+  projectName: "summary", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'zh-Hans',
-    locales: ['zh-Hans'],
+    defaultLocale: "zh-Hans",
+    locales: ["zh-Hans"],
   },
 
   markdown: {
     mermaid: true,
   },
-  themes: ['@docusaurus/theme-mermaid'],
+  themes: ["@docusaurus/theme-mermaid"],
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
           // put the docs to the rott directory
-          routeBasePath: '/', 
-          sidebarPath: './sidebars.ts',
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Halcyon666/summary/blob/main/',
+          editUrl: "https://github.com/Halcyon666/summary/blob/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -64,60 +63,60 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'halcyon666',
+      title: "halcyon666",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/favicon.png',
+        alt: "My Site Logo",
+        src: "img/favicon.png",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'blog',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "blog",
         },
-        /* {to: '/blog', label: 'Blog', position: 'left'}, */
+        // {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/Halcyon666/summary',
-          label: 'GitHub',
-          position: 'right',
-        }
+          href: "https://github.com/Halcyon666/summary",
+          label: "GitHub",
+          position: "right",
+        },
       ],
     },
     algolia: {
-        // The application ID provided by Algolia
-        appId: 'T5OO103S1G',
-  
-        // Public API key: it is safe to commit it
-        apiKey: '8980e7549b3164ed7f36a8394542f0eb',
-  
-        indexName: 'halcyon666',
-  
-        // Optional: see doc section below
-        contextualSearch: true,
-  
-        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-        // externalUrlRegex: 'external\\.com|domain\\.com',
-  
-        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-        replaceSearchResultPathname: {
-          from: '/docs/', // or as RegExp: /\/docs\//
-          to: '/',
-        },
-  
-        // Optional: Algolia search parameters
-        searchParameters: {},
-  
-        // Optional: path for search page that enabled by default (`false` to disable it)
-        searchPagePath: 'search',
-  
-        //... other Algolia params
+      // The application ID provided by Algolia
+      appId: "T5OO103S1G",
+
+      // Public API key: it is safe to commit it
+      apiKey: "8980e7549b3164ed7f36a8394542f0eb",
+
+      indexName: "halcyon666",
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      // externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: "/docs/", // or as RegExp: /\/docs\//
+        to: "/",
       },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: "search",
+
+      //... other Algolia params
+    },
     footer: {
-      style: 'dark',
- /*      links: [
+      style: "dark",
+      /*      links: [
         {
           title: 'Docs',
           items: [
@@ -165,9 +164,9 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
     tableOfContents: {
-        minHeadingLevel: 2,
-        maxHeadingLevel: 5,
-      },
+      minHeadingLevel: 2,
+      maxHeadingLevel: 5,
+    },
   } satisfies Preset.ThemeConfig,
 };
 
