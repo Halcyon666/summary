f95c29384e3f92ad38fe90ab7e79b64c7b414a0e
Halcyon666 <40227502+Halcyon666@users.noreply.github.com>
2024-01-03 01:57:58 +0800

    更新 docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,10 +9,10 @@ const config: Config = {
   favicon: "img/favicon.png",
 
   // Set the production url of your site here
-  url: "http://halcyon666.top/summary/",
+  url: "http://halcyon666.top/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/summary/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
