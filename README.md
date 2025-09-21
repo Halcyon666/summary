6c177ac23091ee8601189e5fbc49013b3122ecec
Halcyon666 <whalefallcl@gmail.com>
2025-09-21 12:51:35 +0800

    支持背景色高亮

--- README.md
+++ README.md
@@ -50,7 +50,7 @@ If you are using GitHub pages for hosting, this command is a convenient way to b
 
 ## forse refesh npm packages
 
-```
+```shell
 pnpm update --latest
 ```
 
--- src/components/MyHighLight.tsx
+++ src/components/MyHighLight.tsx
@@ -2,16 +2,24 @@ import React from "react";
 
 interface HighlightProps {
   color?: string; // 自定义文字颜色
-  className?: string; // 可选，直接使用内置类名，如 "text--danger"
+  background?: string; // 自定义背景色
+  className?: string; // 可选，使用 Docusaurus 内置类，如 "text--danger"
   children: React.ReactNode;
 }
 
 export default function MyHighlight({
   color,
+  background,
   className,
   children,
 }: HighlightProps) {
-  const style = color ? { color } : undefined;
+  const style = {
+    color,
+    background,
+    padding: background ? "0 0.2em" : undefined, // 如果有背景色，加点内边距
+    borderRadius: background ? "0.2em" : undefined, // 圆角
+  };
+
   return (
     <span className={className} style={style}>
       {children}
